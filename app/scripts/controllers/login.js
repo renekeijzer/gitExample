daaf6cf6d13cd1bfdaffc0bc0c96c7ba8314d628
Rene <rene keijzer>
2016-04-17 17:44:40 +0200

    added register

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gitexampleApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the gitexampleApp
+ */
+angular.module('gitexampleApp')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/register.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gitexampleApp.controller:RegisterCtrl
+ * @description
+ * # RegisterCtrl
+ * Controller of the gitexampleApp
+ */
+angular.module('gitexampleApp')
+  .controller('RegisterCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
