2099a6cc34afb918338c9125a5a16e0783560b11
Rene <rene keijzer>
2016-04-17 17:41:05 +0200

    added initial project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gitexampleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gitexampleApp
+ */
+angular.module('gitexampleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gitexampleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gitexampleApp
+ */
+angular.module('gitexampleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
